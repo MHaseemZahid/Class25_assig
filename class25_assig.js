36505f786fda21b8f45f698e42ad89a415b71713
MHaseemZahid <HASEEM.ZAHID@GMAIL.COM>
2024-11-19 23:49:15 +0500

    alter in js

---
+++ class25_assig.js
@@ -0,0 +1,7 @@
+alert("Error! Please enter a valid password");
+alert("Welcome to JS land... \n Happy Coding!");
+
+var b = ("Happy Coding..\n prevent this page form creating additional diologs");
+var c = ("Hello.. I can run JS through my web browser's console");
+alert(b);
+alert(c);
